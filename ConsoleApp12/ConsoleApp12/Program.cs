4fa1f6dfecdd578cf9f026d2b6c3ff8f1cf39afa
VilgotPersson <69978046+VilgotPersson@users.noreply.github.com>
2020-11-18 14:59:03 +0100

    Lade till konsolversionen

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            int datorTal = slump.Next(0, 101);
+            int gissningar = 1;
+            Console.WriteLine("Gissa ett tal mellan 1 och 100");
+            int Tal = int.Parse(Console.ReadLine());
+
+            while (Tal != datorTal)
+            {
+
+                if (Tal < datorTal)
+                {
+                    Console.WriteLine("Ditt tal är mindre än det slumpade talet, gissa igen!");
+                    Tal = int.Parse(Console.ReadLine());
+                    gissningar++;
+                }
+
+                else if (Tal > datorTal)
+                {
+                    Console.WriteLine("Ditt tal är större än det slumpade talet, gissa igen!");
+                    Tal = int.Parse(Console.ReadLine());
+                    gissningar++;
+                }
+
+            }
+                Console.WriteLine("Du gissade rätt tal! Talet var " + datorTal + " Du behövde " + gissningar + " gissningar för att gissa rätt tal");
+        }
+    }
+}
