864d5d29987f0dcd8b7ff1ae260d1daf7d0edba2
VilgotPersson <69978046+VilgotPersson@users.noreply.github.com>
2020-11-18 15:18:16 +0100

    Lade till Windows Forms i reposen

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Random slump = new Random(101);
+            int gissning;
+            int antal = 0;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AnvTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
